7fe82abab94e64609513b1c7fbc17ee7a9aca96b
hiro-mu <wad.akico.0926@icloud.com>
2021-03-12 09:47:41 +0900

    html()の書き換え（jquery →javascript)

--- app/javascript/highlight.js
+++ app/javascript/highlight.js
@@ -19,29 +19,28 @@ const highlight = () => {
     const item = XHR.response.post;
     for (let i=0, len=item.length; i<len; i++){
       const txt = document.getElementById('page-text-content').innerHTML;
-      $('.page-text-content').html(
-        txt.replaceAll(item[i],
-            `<span style="background-color:#FFF450", class="highlight-text">${item[i]}<a href="/books/${bookId}/pages/${pageNum}/search?search=${item[i]}", class="search-btn">
-            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AA
-            ADy0lEQVRYR7WWXWgcVRTH/2e29MEv9EHrB4hYPyCyd1Mtmp1pbFXwQYmo2TttqS/2xRerFPoQFTEB
-            jYrogx8PKmKEFurMVqF99KNVuzdRWrozixbtgh8PjRURNYKQZubITDKbmc3M7mTN3re55+t3zz3nzi
-            F0Wbrllpl4hEBbQdgAxgYA8wScYaAJn5u+70/N7Nz0UzdfaXLKMjJsZ5gZT4JoNIfjPwmYYvY+UOam
-            eg79lkoqgG43XgD46dU4iunuVlK8n9d2BYBhOceZyIg7IOBXAHWQVmf2nEBGVCiB/UEAgwxcmdAnvF
-            SriKfyQCQAdNs9A+CGNsPnlRTPdnJm2M4rDNqX0GE6pMxipRtEC0C33dcB7IkMGDi5Tls38tXowGw3
-            J4G8bNVvJ2iHw0JdXmNKipc72YcAYcGBvowUCfi4JsXDeQK36+i2ew7AFUv7c+DCncq8JbMwQwDdcq
-            rxar+AL7z0U3PjX70AlC23SAS3dRiiI7VK8YEsXzR00LlVK9DJZQPsqVXEm70Ej2zKljtGhBejb4+o
-            9HWl2IJKFGzZqo8RaZHyb0qK+B32zBG/CiY8Pl0Rb6U5I912a8EtLAk/V1Lc03PUmKFuu58BuDvYYu
-            DYtBR3pQNU3SYYG5eEbygpnlgjgOWuYv5BmaWbszIwB+CiQEjAIzUpDqwFgGG7uxjYH3WDkuKSLIC/
-            AVzcZ4CzSopr0gEs53sQ3dTPK6AOrUhl2z1KwLZ+FiF8nlDbS+OpGShXG7uJ+b0lYV/aUGNsPm6K1l
-            uTeAe2WCeu9Wn9z8ubtE/J4qv/pxD1D51xaPRc6INwWlXEQOZLGAiMauMwM49ESgvM139jln7sBWLI
-            PrVNQ+Fo7ECTShaf6Qiw0ghNJcWNvQDotssxu3nfOz84s+O20x0BAmEibYvaC0TYm/e/YNiNSQa3DS
-            E0rmRxotNBEgOJYbsfMfBQokiIjmDBn6ztKM2kOTIOOkNc0CYAvjcpTwbXq+7b8Hi2vRtWjGQpmVj0
-            Swim3mPw8UX4rWErgvZlXLcSLBm8bLuSACvUYzyqTDEV2aQPpVbjfhC/A+DqVdbBPEBB0bXSvuWAe5
-            m/Hn/E/TBgTkthL54rY4WDBfNoq506kzQBsnxvfn+84Iaqzh0aU+rVRRCZAPF4QQo1QPrA5WH2ieYY
-            PAumswx8Mi2Lqp3PqLp7mfFaNjf/68O/LxfAaq5h+NB3V3kL5x/LmTm15gABbMq7knWGf/oCkB+Cfu
-            kbQB4IAuy+AnSDUFJQ3wECiGHr2wGPvHcBCBB+B8NRUjwYyP4Dkrl9Q5viKrwAAAAASUVORK5CYII="></a></span>`
-        )
-      );
+      const replaceTxt = txt.replaceAll(item[i],
+          `<span style="background-color:#FFF450", class="highlight-text">${item[i]}<a href="/books/${bookId}/pages/${pageNum}/search?search=${item[i]}", class="search-btn">
+          <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AA
+          ADy0lEQVRYR7WWXWgcVRTH/2e29MEv9EHrB4hYPyCyd1Mtmp1pbFXwQYmo2TttqS/2xRerFPoQFTEB
+          jYrogx8PKmKEFurMVqF99KNVuzdRWrozixbtgh8PjRURNYKQZubITDKbmc3M7mTN3re55+t3zz3nzi
+          F0Wbrllpl4hEBbQdgAxgYA8wScYaAJn5u+70/N7Nz0UzdfaXLKMjJsZ5gZT4JoNIfjPwmYYvY+UOam
+          eg79lkoqgG43XgD46dU4iunuVlK8n9d2BYBhOceZyIg7IOBXAHWQVmf2nEBGVCiB/UEAgwxcmdAnvF
+          SriKfyQCQAdNs9A+CGNsPnlRTPdnJm2M4rDNqX0GE6pMxipRtEC0C33dcB7IkMGDi5Tls38tXowGw3
+          J4G8bNVvJ2iHw0JdXmNKipc72YcAYcGBvowUCfi4JsXDeQK36+i2ew7AFUv7c+DCncq8JbMwQwDdcq
+          rxar+AL7z0U3PjX70AlC23SAS3dRiiI7VK8YEsXzR00LlVK9DJZQPsqVXEm70Ej2zKljtGhBejb4+o
+          9HWl2IJKFGzZqo8RaZHyb0qK+B32zBG/CiY8Pl0Rb6U5I912a8EtLAk/V1Lc03PUmKFuu58BuDvYYu
+          DYtBR3pQNU3SYYG5eEbygpnlgjgOWuYv5BmaWbszIwB+CiQEjAIzUpDqwFgGG7uxjYH3WDkuKSLIC/
+          AVzcZ4CzSopr0gEs53sQ3dTPK6AOrUhl2z1KwLZ+FiF8nlDbS+OpGShXG7uJ+b0lYV/aUGNsPm6K1l
+          uTeAe2WCeu9Wn9z8ubtE/J4qv/pxD1D51xaPRc6INwWlXEQOZLGAiMauMwM49ESgvM139jln7sBWLI
+          PrVNQ+Fo7ECTShaf6Qiw0ghNJcWNvQDotssxu3nfOz84s+O20x0BAmEibYvaC0TYm/e/YNiNSQa3DS
+          E0rmRxotNBEgOJYbsfMfBQokiIjmDBn6ztKM2kOTIOOkNc0CYAvjcpTwbXq+7b8Hi2vRtWjGQpmVj0
+          Swim3mPw8UX4rWErgvZlXLcSLBm8bLuSACvUYzyqTDEV2aQPpVbjfhC/A+DqVdbBPEBB0bXSvuWAe5
+          m/Hn/E/TBgTkthL54rY4WDBfNoq506kzQBsnxvfn+84Iaqzh0aU+rVRRCZAPF4QQo1QPrA5WH2ieYY
+          PAumswx8Mi2Lqp3PqLp7mfFaNjf/68O/LxfAaq5h+NB3V3kL5x/LmTm15gABbMq7knWGf/oCkB+Cfu
+          kbQB4IAuy+AnSDUFJQ3wECiGHr2wGPvHcBCBB+B8NRUjwYyP4Dkrl9Q5viKrwAAAAASUVORK5CYII="></a></span>`
+      )
+      document.getElementById('page-text-content').innerHTML = replaceTxt;
     }  
   }
 
